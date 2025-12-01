f45c90ae9b15e665eb6fa37c17732df89a48d6df
lnb <lnbsa12345@gmail.com>
2025-12-02 03:41:08 +0530

    cleanups

--- 24/Day 7/aoc7.c
+++ 24/Day 7/aoc7.c
@@ -1,22 +1,17 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef uint64_t ull;
 #define N 850
+
+typedef uint64_t ull;
 typedef unsigned char uc;
-
-// bitmasked, handles all operators
 uc check(int *, int, int, ull, ull);
-
-#include <stdio.h>
 
 int main() {
   FILE *f = fopen("input", "r");
-  if (!f) {
-    fprintf(stderr, "input file missing\n");
-    return 1;
-  }
+  assert(f && "input file missing");
   ull p1 = 0, p2 = 0;
   for (int i = 0; i < N; ++i) {
     ull t;
@@ -30,11 +25,9 @@ int main() {
     }
     fseek(f, -offset, SEEK_CUR);
     int *arr = malloc(n * sizeof(int));
-    int j = 0;
-    for (; j < n - 1; ++j)
-      fscanf(f, "%d ", arr + j);
-    fscanf(f, "%d\n", arr + j);
-    uc bits = check(arr, n, 1, arr[0], t);
+    for (int j = 0; j < n; ++j)
+      fscanf(f, " %d", arr + j);
+    uc bits = check(arr, n, 1, *arr, t);
     if (bits & 1)
       p1 += t;
     if (bits & 2)
@@ -42,6 +35,7 @@ int main() {
     free(arr);
   }
   printf("p1: %lu\np2: %lu\n", p1, p2);
+  fclose(f);
   return 0;
 }
 
@@ -55,11 +49,8 @@ uc check(int *arr, int n, int idx, ull v, ull t) {
   if (add <= t)
     ret |= check(arr, n, idx + 1, add, t);
 
-  if (arr[idx] != 0 && v <= t / arr[idx]) {
-    ull mul = v * arr[idx];
-    if (mul <= t)
-      ret |= check(arr, n, idx + 1, mul, t);
-  }
+  if (v <= t / arr[idx])
+    ret |= check(arr, n, idx + 1, v * arr[idx], t);
 
   int tmp = arr[idx], p = 1;
   while (tmp > 0) {
@@ -67,9 +58,8 @@ uc check(int *arr, int n, int idx, ull v, ull t) {
     tmp /= 10;
   }
   v = v * p + arr[idx];
-  if (v <= t)
-    if (check(arr, n, idx + 1, v, t))
-      ret |= 2;
+  if (v <= t && check(arr, n, idx + 1, v, t))
+    ret |= 2;
 
   return ret;
 }
