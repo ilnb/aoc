a1f6d943d768691b8c0c0981266c9262a060f99b
lnb <lnbsa12345@gmail.com>
2026-05-06 18:02:37 +0530

    syntax

--- 25/Day 8/src/main.zig
+++ 25/Day 8/src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const DSU = @import("dsu").DSU;
 const N = 1000;
 
-const Point = struct { x: u32, y: u32, z: u32 };
+const Point = @Tuple(&.{ u32, u32, u32 });
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
@@ -25,32 +25,29 @@ pub fn main(init: std.process.Init) !void {
         const x = try std.fmt.parseInt(u32, (it.next()).?, 10);
         const y = try std.fmt.parseInt(u32, (it.next()).?, 10);
         const z = try std.fmt.parseInt(u32, (it.next()).?, 10);
-        lines[i] = .{ .x = x, .y = y, .z = z };
+        lines[i] = .{ x, y, z };
     }
 
-    const Data = struct {
-        d: u64,
-        u: usize,
-        v: usize,
-        fn lt(_: void, a: @This(), b: @This()) std.math.Order {
-            return std.math.order(a.d, b.d);
-        }
-    };
+    const Data = @Tuple(&.{ u64, usize, usize });
 
-    var pq = std.PriorityQueue(Data, void, Data.lt).empty;
+    var pq = std.PriorityQueue(Data, void, struct {
+        fn lt(_: void, a: Data, b: Data) std.math.Order {
+            return std.math.order(a.@"0", b.@"0");
+        }
+    }.lt).empty;
     defer pq.deinit(ga);
 
     for (0..N) |i| for (i + 1..N) |j| {
         const d = dist(lines[i], lines[j]);
-        try pq.push(ga, .{ .d = d, .u = i, .v = j });
+        try pq.push(ga, .{ d, i, j });
     };
 
     var dsu = try DSU(usize).init(ga, N);
     defer dsu.deinit();
 
     for (0..N) |_| {
-        const t = pq.pop().?;
-        dsu.join(t.u, t.v);
+        _, const u, const v = pq.pop().?;
+        dsu.join(u, v);
     }
 
     var size_pq = std.PriorityQueue(usize, void, struct {
@@ -70,10 +67,11 @@ pub fn main(init: std.process.Init) !void {
 
     var p2: u64 = 0;
     while (pq.pop()) |t| {
-        dsu.join(t.u, t.v);
+        _, const u, const v = t;
+        dsu.join(u, v);
         if (dsu.ncomps == 1) {
-            const x1 = lines[t.u].x;
-            const x2 = lines[t.v].x;
+            const x1 = lines[u].@"1";
+            const x2 = lines[v].@"1";
             p2 = x1 * x2;
             break;
         }
@@ -87,8 +85,8 @@ pub fn main(init: std.process.Init) !void {
 }
 
 fn dist(p1: Point, p2: Point) u64 {
-    const x1, const y1, const z1 = .{ p1.x, p1.y, p1.z };
-    const x2, const y2, const z2 = .{ p2.x, p2.y, p2.z };
+    const x1, const y1, const z1 = p1;
+    const x2, const y2, const z2 = p2;
 
     const dx = @as(i64, x1) - @as(i64, x2);
     const dy = @as(i64, y1) - @as(i64, y2);
